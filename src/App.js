8362d1933000d1b9255397abd037cb453245df5b
tanayraikuwar <tanayraikuwar26@gmail.com>
2023-02-02 23:17:53 +0530

    some minor changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
 import Template1 from "./Template/Template1/template1";
 import Template2 from "./Template/Template2/template2";
+import Form from "./Components/multistepForm/Form";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/about_us" element={<About />} />
         <Route exact path="/template1" element={<Template1 />} />
         <Route exact path="/template2" element={<Template2 />} />
+        <Route exact path="/myresume/changePersonalDetails" element={<Form/>}/>
       </Routes>
     </>
   );
