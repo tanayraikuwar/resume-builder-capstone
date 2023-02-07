1716e9c3cf817578bd010ae294b66b92a707e516
tanayraikuwar <tanayraikuwar26@gmail.com>
2023-02-07 18:28:03 +0530

    changes to display hamberger menu in smaller displays

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,67 +6,37 @@ import {useState} from 'react';
 
 
 
-
-
-
 function Navbar() {
-
-  // to change burger classes
- const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
- const [menu_class, setMenuClass] = useState("menu hidden")
- const [isMenuClicked, setIsMenuClicked] = useState(false)
-
- //toggle burger menu change
- const updateMenu = () => {
-if(!isMenuClicked) {
-  setBurgerClass("burger-bar clicked")
-  setMenuClass("menu visible")
-}
-else{
-  setBurgerClass("burger-bar unclicked")
-  setMenuClass("menu hidden")
-}
-setIsMenuClicked(!isMenuClicked)
-}
-
+  const[activeState,setActiveState] = useState(false);
+  // we use "useState" to toggle between two classes
+  let activeStateToggler = () => setActiveState(!activeState);
+  // function for toggling
   return (
-    <div className='mainNavbar'>
-      <header className="header">
+    <header className="header">
+
+      <nav className="navbar" id='navbar'>
+
         <NavLink exact to="/"><img src={alma} alt="almabetter" className="logo" /></NavLink>
-        
-        <nav className="navbar" id='navbar'>
-          
-          <ul className="navbar-list m-3">   {/** Adding some bootstrap classes to add margin */}
-            
-            <li><NavLink exact style={({ isActive }) => ({
-              color: isActive ? 'rgb(20, 122, 218)' : 'rgb(112, 111, 111)'
-            })} className={"navbar-link"} to="/" >ResumeTemplate</NavLink></li>
-            
-            <li><NavLink exact style={({ isActive }) => ({
-              color: isActive ? 'rgb(20, 122, 218)' : 'rgb(112, 111, 111)'
-            })} className={"navbar-link"} to="/my_resume" >My Resume</NavLink></li>
-            
-            <li><NavLink exact style={({ isActive }) => ({
-              color: isActive ? 'rgb(20, 122, 218)' : 'rgb(112, 111, 111)'
-            })} className={"navbar-link"} to="/about_us" >About Us</NavLink></li>
-          </ul>
-          <div className='burger-menu' onClick={updateMenu}>
-             <div className={burger_class} ></div>
-             <div className={burger_class} ></div>
-             <div className={burger_class} ></div>
-          </div>
-        </nav>
-        <div className={menu_class}>
-          <ul className='component-items'>
-            <li className='items'>Resume
-            </li>
-            <li className='items'>My Resume
-            </li>
-            <li className='items'>About Us
-            </li>
-          </ul>
-        </div>
-      </header></div>
+
+        <i className="bi bi-list" onClick={activeStateToggler}></i> 
+
+        <ul className={activeState ? "navbar-list m-3 active" : "navbar-list m-3"}>   {/** Adding some bootstrap classes to add margin */}
+          {/* the above terenary operator is for toggling between classes */}
+          <li><NavLink exact style={({ isActive }) => ({
+            color: isActive ? 'rgb(20, 122, 218)' : 'rgb(112, 111, 111)'
+          })} className={"navbar-link"} to="/" >ResumeTemplate</NavLink></li>
+
+          <li><NavLink exact style={({ isActive }) => ({
+            color: isActive ? 'rgb(20, 122, 218)' : 'rgb(112, 111, 111)'
+          })} className={"navbar-link"} to="/my_resume" >My Resume</NavLink></li>
+
+          <li><NavLink exact style={({ isActive }) => ({
+            color: isActive ? 'rgb(20, 122, 218)' : 'rgb(112, 111, 111)'
+          })} className={"navbar-link"} to="/about_us" >About Us</NavLink></li>
+        </ul>
+
+      </nav>
+    </header>
   )
 }
 
