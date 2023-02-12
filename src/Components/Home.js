2756c1cfc5aa3f5bc7f9988927fa00a40aaa9652
tanayraikuwar <tanayraikuwar26@gmail.com>
2023-02-12 17:18:21 +0530

    changes to select template according to click of button 'useTemplate'

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,11 +5,14 @@ import resume2 from "./Images/resume2.jpg"
 import resume3 from "./Images/resume3.jpg"
 import resume4 from "./Images/resume4.jpg"
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import selectTemplate from "../Redux/Action/templateActions/selectTemplate";
 
 
 
 
 const Home = () => {
+  let dispatch = useDispatch();
 
   return (
     <div className="mainHome">
@@ -21,34 +24,30 @@ const Home = () => {
         <div className="grid-container">
 
           <div className="grid-item grid-item-1">
-          <Link to={"/myresume/changePersonalDetails"}> <button className="Mybtn btn btn-primary" onClick={
-            console.log("template2")
-          }> Use Template</button>
+          <Link to={"/myresume/changePersonalDetails"}> <button className="Mybtn btn btn-primary" 
+          onClick={()=>dispatch(selectTemplate("template1"))}> Use Template</button>
           </Link>
             <img src={resume} alt="resume" />
             
           </div>
 
           <div className="grid-item grid-item-2">
-          <Link to={"/myresume/changePersonalDetails"}><button className="Mybtn btn btn-primary" onClick={() => {
-              console.log("template2")
-            }}>Use Template</button></Link>
+          <Link to={"/myresume/changePersonalDetails"}><button className="Mybtn btn btn-primary"
+          onClick={()=>dispatch(selectTemplate("template2"))} >Use Template</button></Link>
             <img src={resume2} alt="resume" />
           
           </div>
 
           <div className="grid-item grid-item-3">
-          <Link to={"/myresume/changePersonalDetails"}><button className="Mybtn btn btn-primary" onClick={() => {
-              console.log("template3")
-            }}>Use Template</button></Link>
+          <Link to={"/myresume/changePersonalDetails"}><button className="Mybtn btn btn-primary"
+          onClick={()=>dispatch(selectTemplate("template3"))} >Use Template</button></Link>
             <img src={resume3} alt="resume" />
            
           </div>
 
           <div className="grid-item grid-item-4">
-          <Link to={"/myresume/changePersonalDetails"}><button className="Mybtn btn btn-primary" onClick={() => {
-              console.log("template4")
-            }}>Use Template</button></Link>
+          <Link to={"/myresume/changePersonalDetails"}><button className="Mybtn btn btn-primary"
+          onClick={()=>dispatch(selectTemplate("template4"))} >Use Template</button></Link>
             <img src={resume4} alt="resume" />
           
           </div>
